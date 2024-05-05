e41c035288954cd6969fa5ca3b8cefc5453570a1
Mrugendra Shilvant <mrugendra.shilvant@digio.in>
2024-05-05 16:26:37 +0530

    connected mongodb to node

--- backend/rest.js
+++ backend/rest.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://mrugendrashilvant16:4sf1RqPBT1vYUPiX@cluster0.km61tdu.mongodb.net/blogData")
+.then(()=>{
+    console.log("MongoDB Connection Success");
+})
+.catch((err)=>{
+    console.log("MongoDB Connection Failed!");
+    throw new Error(err);
+})
 
 const app = express();
 
