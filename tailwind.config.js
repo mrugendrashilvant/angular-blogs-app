aac2817c3eb799b6eeb0fe6ab6d7814d759c9d0c
Mrugendra Shilvant <mrugendra.shilvant@digio.in>
2024-05-05 18:43:05 +0530

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          md: '3.5rem',
+          lg: '5rem',
+          xl: '6rem',
+          '2xl': '7rem',
+        }
+      },
+    },
   },
   plugins: [],
 }
