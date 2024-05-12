6aff7061020e9ec558bbf5f8336ef6ecbfd6d2d4
Mrugendra Shilvant <mrugendra.shilvant@digio.in>
2024-05-12 16:08:30 +0530

    form completed

--- src/app/components/create-blog/create-blog.component.ts
+++ src/app/components/create-blog/create-blog.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-create-blog',
@@ -9,9 +13,25 @@ import {MatDividerModule} from '@angular/material/divider';
   imports: [
     CommonModule,
     MatDialogModule,
-    MatDividerModule
+    MatDividerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   templateUrl: './create-blog.component.html',
   styleUrl: './create-blog.component.scss',
 })
-export class CreateBlogComponent { }
+export class CreateBlogComponent {
+  blogForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    tags: new FormControl('', Validators.required),
+    content: new FormControl('', Validators.required),
+    image: new FormControl(null, Validators.required)
+  });
+
+  onSubmit(ev:SubmitEvent) {
+    console.log(ev);
+  }
+}
