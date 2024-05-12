9acac8eadabe282e234548743c8b1a035dc4cce3
Mrugendra Shilvant <mrugendra.shilvant@digio.in>
2024-05-12 15:15:22 +0530

    button to create blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HttpClientModule } from '@angular/common/http';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, MatSlideToggleModule, HttpClientModule, MatToolbarModule]
+    imports: [CommonModule, RouterOutlet, MatSlideToggleModule, HttpClientModule, MatToolbarModule, MatButtonModule]
 })
 export class AppComponent {
   title = 'My new blogs app!';
